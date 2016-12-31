f13512cd134e29e5ddd23584e8fa3989f43f07ee
YoungLeeNENU <youngleemails@gmail.com>
2016-12-31 12:52:54 +0800

    updates

--- src/client/js/common/string.js
+++
@@ -1,6 +0,0 @@
-/**
- * @fileOverview 字符串处理
- * @name string.js
- * @author Young Lee youngleemails@gmail.com
- * @license GPL
- */
--- src/client/js/emacs/control.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-	var a = 5;
-
-	// $('#L_keyOp').emacsListenKeyBind({
-	// });
-	// $(window).keydown(function (event) {
-	// 	console.log(event.keyCode);
-	// });
-});
