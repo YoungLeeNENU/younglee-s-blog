d58284dc01111750c20f91e18c9f4277981cbeb3
YoungLeeNENU <youngleemails@gmail.com>
2017-01-16 11:18:26 +0800

    解决了中文编码问题

--- src/server/middleware/file_sniffer.py
+++ src/server/middleware/file_sniffer.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# @brief: 单独开辟一个线程，做文件嗅探使用
+# @brief: 单独开辟一个(守护)线程，做文件嗅探使用
+# TODO: 文件夹：增/删/重命名
+# TODO: 文件：增/删/重命名
+# TODO: 线程池优雅控制的问题
+# TODO: 如何关闭守护线程的问题
 # @author: Young Lee
 # @email: youngleemails@gmail.com
 # @time: 01-03-2017
 # @license GPL V3
-import os, sys
+from __future__ import unicode_literals
+import os
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+import codecs
 import hashlib
 import traceback
 import threading
 from copy import deepcopy
 from time import sleep, ctime
+from pprint import pprint
+import uniout
 
 root = '/var/ylpn/ylpn-data/'
 
@@ -40,7 +51,6 @@ class FileSniffer():
             try:
                 print '... Sniffering ...'
                 record = self._walk(dirpath) # 一次 walk 的结果
-                # print record
                 if self.files_record == record:
                     pass
                 else:
@@ -49,26 +59,25 @@ class FileSniffer():
                 sleep(1)
             except Exception:
                 break
-    # def dft_file_change_callback():
-    #     """
-    #     [yas] elisp error: Symbol's value as variable is void: text
-    #     """
-    #     pass
     def _diff(self, cache, record):
         """
-        检查最新更新的文件
+        检查最新更新的文件和文件夹
         """
-        print record
+        print "... Diffing ..."
+        pprint (cache)
+        pprint (record)
         _cache, _record = deepcopy(cache), deepcopy(record)
         _cache.reverse()
         _record.reverse()
-        print "... Diffing ..."
     def _walk(self, dirpath):
         """
         遍历指定的路径
         """
         files_record = []
         def path_adjust(raw_path):
+            """
+            处理 walk 结果的路径末尾的 / 问题
+	    """
             if raw_path[-1:] == '/': return raw_path
             else: return raw_path + '/'
         def exclude_rule(name):
@@ -80,14 +89,14 @@ class FileSniffer():
             return True
         try:
             for e in os.walk(dirpath):
-                # print e
                 dirpath, dirnames, raw_filenames, filenames = e[0], e[1], filter(exclude_rule, e[2]), []
                 for f in raw_filenames:
                     fp = open(path_adjust(dirpath) + f, 'rb')
                     md5obj = hashlib.md5()
                     md5obj.update(fp.read())
                     hash_key = md5obj.hexdigest() # 文件哈希
-                    file_info = { 'filename': dirpath + '/' + f,
+                    # file_info = { 'filename': (path_adjust(dirpath) + f).decode('utf8'),
+                    file_info = { 'filename': path_adjust(dirpath) + f,
                                   'md5': hash_key }
                     filenames.append(file_info)
                 files_record.append({ 'dirpath': dirpath,
@@ -102,6 +111,7 @@ class DaemonicFileSniffer():
     """
     def __init__(self, ):
         """
+        TODO 线城池的控制这里弱智了一点
         """
         self.f_sniffer = FileSniffer()
         self.thread = []        # 线程池
@@ -128,7 +138,7 @@ class DaemonicFileSniffer():
 if __name__ == '__main__':
     f_sniffer = FileSniffer()
     f_sniffer.sniffering(root)
+
     # file_sniffer = DaemonicFileSniffer()
     # file_sniffer.start(False)
-
     # file_sniffer.join(4)
